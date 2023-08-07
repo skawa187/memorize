1f326bc8aa12aff283c77c07b772ab3726ac1faf
Slawomir Kawa <skawa@gmail.com>
2023-08-07 18:57:43 +0200

    implement CRUD functionality

--- memo/urls.py
+++ memo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MemoList, MemoDetail
+from .views import MemoList, MemoDetail, MemoCreate, MemoUpdate, MemoDelete
 
 urlpatterns = [
-    path('', MemoList.as_view(), name = 'memo_list'),
-    path('memo/<int:pk>/', MemoDetail.as_view(), name = 'memo_detail')
+    path('', MemoList.as_view(), name = 'memo-list'),
+    path('memo/<int:pk>/', MemoDetail.as_view(), name = 'memo-detail'),
+    path('memo-create/', MemoCreate.as_view(), name = 'memo-create'),
+    path('memo-update/<int:pk>', MemoUpdate.as_view(), name = 'memo-update'),
+    path('memo-delete/<int:pk>', MemoDelete.as_view(), name = 'memo-delete'),
 ]
--- memo/views.py
+++ memo/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 from .models import Memo
 
 class MemoList(ListView):
     model = Memo
     template_name = 'memo/memos.html'
+    context_object_name = 'memo_list'
 
 class MemoDetail(DetailView):
     model = Memo
     template_name = 'memo/memo-detail.html'
     context_object_name = 'memo'
+
+class MemoCreate(CreateView):
+    model = Memo
+    template_name = 'memo/memo-create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('memo-list')
+
+class MemoUpdate(UpdateView):
+    model = Memo
+    template_name = 'memo/memo-create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('memo-list')
+
+class MemoDelete(DeleteView):
+    model = Memo
+    template_name = 'memo/memo-delete.html'
+    context_object_name = 'memo'
+    success_url = reverse_lazy('memo-list')
